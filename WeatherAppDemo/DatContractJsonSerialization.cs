07927a52ad2a822446a627bbdae05c6b11894d4f
anhlhd00587 <anhlhd00587@fpt.edu.vn>
2018-10-25 09:04:49 +0700

    WeatherAppDemo

---
+++ WeatherAppDemo/DatContractJsonSerialization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WeatherAppDemo
+{
+    internal class DatContractJsonSerialization
+    {
+        private Type type;
+
+        public DatContractJsonSerialization(Type type)
+        {
+            this.type = type;
+        }
+    }
+}
